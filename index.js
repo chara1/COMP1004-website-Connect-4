4ebd232ec8a32a442f8ca2d8916f55dcba4ff796
chara1 <saket.nimkar@gmail.com>
2024-04-11 00:57:08 +0100

    Game tiles and game pieces
    Game tiles and game pieces have been made

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var PlayerRed = "R";
 var PlayerYellow = "Y";
 var currPlayer = "PlayerRed";
 
-var gameOver = "false";
+var gameOver = false;
 var board;
 
 var rows = 6;
@@ -22,8 +22,34 @@ function setGame() {
             row.push(' ');
             
             //html
+            // <div id="0-0" class="tile"></div> // Appends the dev that has the ID board
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.addEventListener("click", setPiece);
+            document.getElementById("board").append(tile);
         }
+        board.push(row);
+    }
+}
+
+function setPiece(){
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-"); //"0-0" -> ["0", "0"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    board[r][c] = currPlayer;
+    let tile = this;
+    if (currPlayer == PlayerRed) {
+        tile.classList.add("red-piece");
+        currPlayer = PlayerYellow;
+    }
+    else {
+        tile.classList.add("yellow-piece");
+        currPlayer = PlayerRed;
     }
 }
